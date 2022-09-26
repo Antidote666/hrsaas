13a5c137bc121a5539a4c0ac907d801e8ff1a377
Antidote666 <www.25868@qq.com>
2022-09-26 09:23:56 +0800

    feat:登录功能完成

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,20 @@
 import axios from 'axios'
-
+import { Message } from 'element-ui'
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
   itmeout: 5000
+})
+
+service.interceptors.response.use(response => {
+  const { data, message, success } = response.data
+  if (success) {
+    return data
+  }
+  Message.error(message)
+  return Promise.reject(new Error(message))
+}, error => {
+  Message.error(error.message)
+  return Promise.reject(error.message)
 })
 
 export default service
